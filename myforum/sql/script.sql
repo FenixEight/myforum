dcf203266fff83e0f32fb219d84223fdcdc62e47
Evgeniy Soprun <extrema.rs@gmail.com>
2016-06-07 15:01:52 +0300

    sql repair

--- myforum/sql/script.sql
+++ myforum/sql/script.sql
@@ -1,10 +1,5 @@
-
-
-CREATE TABLE "comments"(
-	comment_id int not null default nextval('"seq_comment"'::text) primary key,
-	user_name varchar(128),
-	comment_text text,
-	creation_date timestamp not null);
+CREATE SEQUENCE seq_users MINVALUE 1;
+CREATE SEQUENCE seq_comments MINVALUE 1;
 
 CREATE TABLE "users"(
 id int not null default nextval('"seq_users"'::text) primary key,
@@ -19,16 +14,7 @@ post varchar,
 date_time varchar,
 user_agent varchar,
 ip varchar,
-user id references users(id)
+user_id int references users(id)
 );
 
 
-###
--- Выполнение запроса:
-CREATE SEQUENCE seq_users MINVALUE 1;
-Query returned successfully with no result in 22 msec.
-
--- Выполнение запроса:
-CREATE SEQUENCE seq_comments MINVALUE 1;
-Query returned successfully with no result in 23 msec.
-###
